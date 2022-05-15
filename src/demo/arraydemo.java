55685366c7b08d6208442f324dc28b5ffdcd436f
2390858916 <2390858916@qq.com>
2022-05-15 16:39:16 +0800

    Changes

--- src/demo/arraydemo.java
+++ src/demo/arraydemo.java
@@ -7,12 +7,20 @@ public class arraydemo {
         int[] arr = new int[3];
 //        类型 数组 数组名 申请空间 类型 数组 长度
 //        静态初始化
-        int[] arr2 = {1,2,3};
+        int[] arr2 = {100,200,333};
         arr2[1] = 100;
         System.out.println(arr[1]);
+
         for(int i = 0;i<arr2.length;i++){
             System.out.println(arr[i]);
         }
+        int max = 0;
+        for (int x = 0;x<arr2.length;x++){
+            if(arr2[x]>max){
+                max = arr2[x];
+            }
+        }
+        System.out.println(max);
 
     }
 }
