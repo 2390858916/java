97187ff8179b43263d3b77ea71a7f3c8077cb2f9
2390858916 <2390858916@qq.com>
2022-05-15 14:27:39 +0800

    Changes

--- src/demo/arraydemo.java
+++ src/demo/arraydemo.java
@@ -6,6 +6,12 @@ public class arraydemo {
 //        只指定长度 由系统数组分配初始值
         int[] arr = new int[3];
 //        类型 数组 数组名 申请空间 类型 数组 长度
+//        静态初始化
+        int[] arr2 = {1,2,3};
+        System.out.println(arr[1]);
+        for(int i = 0;i<arr2.length;i++){
+            System.out.println(arr[i]);
+        }
 
     }
 }
