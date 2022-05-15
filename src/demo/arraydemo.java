b3bb2a42e72b2669558db0f01dfa9d2ab3a52df2
2390858916 <2390858916@qq.com>
2022-05-15 13:08:58 +0800

    Changes

---
+++ src/demo/arraydemo.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class arraydemo {
+    public static void main(String[] args) {
+//        动态初始化数组
+//        只指定长度 由系统数组分配初始值
+        int[] arr = new int[3];
+//        类型 数组 数组名 申请空间 类型 数组 长度
+    }
+}
--- src/demo/hellow.java
+++ src/demo/hellow.java
@@ -3,5 +3,6 @@ package demo;
 public class hellow {
     public static void main(String[] args){
         System.out.println(111);
+
     }
 }
