01c03ff2f73ef07c2b9df0ead07765d7f9c96c9b
2390858916 <2390858916@qq.com>
2022-05-18 23:12:48 +0800

    Changes

--- src/method/selectarray.java
+++ src/method/selectarray.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class selectarray {
     public static void main(String[] args) {
         int[] arr = {11,22,33};
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int retult = select(arr,x);
+        System.out.println(retult);
 
+    }
+    public static int select(int[] arr,int x){
+        int i = 0;
+        for (i = 0;i<arr.length;i++){
+            if(x==arr[i]){
+               break;
+            }
+        }
+        return i;
     }
 }
