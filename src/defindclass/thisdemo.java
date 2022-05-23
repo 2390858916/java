be240ec80f030552133f5c90e63ab4c665f3866f
2390858916 <2390858916@qq.com>
2022-05-23 10:24:26 +0800

    Changes

---
+++ src/defindclass/thisdemo.java
@@ -0,0 +1,10 @@
+package defindclass;
+
+public class thisdemo {
+    public static void main(String[] args) {
+        thisuse ts = new thisuse();
+        ts.SetName("niubi");
+        ts.Setage(30);
+        ts.show();
+    }
+}
---
+++ src/defindclass/thisuse.java
@@ -0,0 +1,15 @@
+package defindclass;
+
+public class thisuse {
+    private String name;
+    private int age;
+    public void SetName(String name){
+        this.name = name;
+    }
+    public void Setage(int age){
+        this.age = age;
+    }
+    public void show(){
+        System.out.println(name + ":"+ age);
+    }
+}
