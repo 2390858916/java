8ac3d439f1d771390bd0b46bb72792cf71fbb70a
2390858916 <2390858916@qq.com>
2022-05-24 16:44:49 +0800

    Changes

---
+++ src/defindclass/gozao.java
@@ -0,0 +1,12 @@
+package defindclass;
+
+public class gozao {
+    private String name;
+    private int age;
+    public gozao(){
+        System.out.println("have no function");
+    }
+    public void show(){
+        System.out.println(name + ":" + age);
+    }
+}
---
+++ src/defindclass/gozaoDemo.java
@@ -0,0 +1,8 @@
+package defindclass;
+
+public class gozaoDemo {
+    public static void main(String[] args) {
+        gozao gz = new gozao();
+        gz.show();
+    }
+}
