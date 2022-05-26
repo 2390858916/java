1b8b3c7058da2192efd143ebf9e45fe1c592d826
2390858916 <2390858916@qq.com>
2022-05-26 21:58:25 +0800

    Changes

--- src/Arraylist/ArraylistModo.java
+++ src/Arraylist/ArraylistModo.java
@@ -7,7 +7,11 @@ public class ArraylistModo {
         ArrayList<String> array = new ArrayList<String>();
         array.add("hello");
         array.add("world");
+        array.add("java");
+        array.add("555");
         array.remove("world");
+        array.remove(1);
+        array.set(0,"fist");
         System.out.println(array);
     }
 }
