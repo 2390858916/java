6c39b4d1d87bf298e0a0383f050691d29baa5e30
2390858916 <2390858916@qq.com>
2022-05-26 16:36:44 +0800

    Changes

---
+++ src/Arraylist/ArrayDemo.java
@@ -0,0 +1,8 @@
+package Arraylist;
+import java.util.ArrayList;
+public class ArrayDemo {
+    public static void main(String[] args) {
+        ArrayList<String> array = new ArrayList<String>();
+        System.out.println("array" + array);
+    }
+}
