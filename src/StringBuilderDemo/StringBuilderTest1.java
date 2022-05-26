9526b691b4e682375e78735611eab1eadb298d2c
2390858916 <2390858916@qq.com>
2022-05-26 10:36:51 +0800

    Changes

---
+++ src/StringBuilderDemo/StringBuilderTest1.java
@@ -0,0 +1,12 @@
+package StringBuilderDemo;
+
+public class StringBuilderTest1 {
+    public static void main(String[] args) {
+        StringBuilder sb= new StringBuilder();
+        System.out.println(sb);
+        System.out.println(sb.length());
+        StringBuilder sb2 = new StringBuilder("hellow");
+        System.out.println(sb2);
+        System.out.println(sb2.length());
+    }
+}
