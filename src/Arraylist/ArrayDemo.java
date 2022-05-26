f37f727ef9d819d8da301f31d6396ad2ef5ea945
2390858916 <2390858916@qq.com>
2022-05-26 21:32:02 +0800

    Changes

--- src/Arraylist/ArrayDemo.java
+++ src/Arraylist/ArrayDemo.java
@@ -2,7 +2,11 @@ package Arraylist;
 import java.util.ArrayList;
 public class ArrayDemo {
     public static void main(String[] args) {
+        //空集合对象
         ArrayList<String> array = new ArrayList<String>();
         System.out.println("array" + array);
+        array.add("hello");
+        array.add("java");
+        System.out.println(array);
     }
 }
