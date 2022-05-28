2f14b4f38807f9b32cd77550a45682c4b518e4e8
2390858916 <2390858916@qq.com>
2022-05-28 16:40:38 +0800

    Changes

--- src/extendsDemo/son.java
+++ src/extendsDemo/son.java
@@ -2,6 +2,7 @@ package extendsDemo;
 
 public class son extends fu {
     public int height = 75;
+    public int age = 999;
     public void method(){
         System.out.println(age);
         System.out.println(height);
