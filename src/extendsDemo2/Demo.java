98f62378c91871b1efa9313f0747184774bee7d5
2390858916 <2390858916@qq.com>
2022-06-12 16:21:24 +0800

    Changes

---
+++ src/extendsDemo2/Demo.java
@@ -0,0 +1,10 @@
+package extendsDemo2;
+
+public class Demo {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher();
+        t1.setName("666");
+        t1.setAge(60);
+        System.out.print(t1.getName() + t1.getAge());
+    }
+}
